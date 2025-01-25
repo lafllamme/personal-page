4a8fdca79f1ab5525afd10f4600b863c7fb2d3f4
Flame <teke900@gmail.com>
2025-01-25 17:51:11 +0100

    feat: update main app with Nuxt default components
    Replaced the custom button with Nuxt's built-in loading indicator and page rendering components for better integration and maintainability. This simplifies the structure and leverages the framework's native functionality.

--- app.vue
+++ app.vue
@@ -1,10 +1,6 @@
 <template>
-  <div class="flex justify-center pt-40">
-    <button class="flex justify-center space-x-2 items-center uppercase focus:ring-4 focus-visible:outline-none focus:ring-gray-800 focus:ring-offset-2 bg-amber-600 rounded-lg text-white px-4 py-2 font-thin text-2xl font-mono antialiased">
-        <span>Click me</span>
-        <Icon name="uil:github" style="color: black" />
-    </button>
-  </div>
+  <NuxtLoadingIndicator :throttle="0" />
+ <NuxtPage/>
 </template>
 <script setup lang="ts">
 </script>
