b9b20e3da8e3fbc5cb2c3865867433af2b45f2d4
Flame <teke900@gmail.com>
2025-01-25 17:17:47 +0100

    feat: Add styled button with GitHub icon to app layout
    Replaced the plain GitHub icon with a styled button featuring a "Click me" label and associated icon. The button includes accessibility and styling improvements, enhancing the user interface. Added TypeScript setup for potential JavaScript usage.

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <Icon name="uil:github" style="color: black" />
+  <div class="flex justify-center pt-40">
+    <button class="flex justify-center space-x-2 items-center uppercase focus:ring-4 focus-visible:outline-none focus:ring-gray-800 focus:ring-offset-2 bg-amber-600 rounded-lg text-white px-4 py-2 font-thin text-2xl font-mono antialiased">
+        <span>Click me</span>
+        <Icon name="uil:github" style="color: black" />
+    </button>
   </div>
 </template>
+<script setup lang="ts">
+</script>
