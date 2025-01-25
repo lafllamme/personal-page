2ae977aeed9071c02751855694d9817988293cc9
Flame <teke900@gmail.com>
2025-01-25 17:52:39 +0100

    feat: Add homepage with navigation and basic layout
    Introduce a new `index.vue` file for the homepage, including a button to navigate to the "About" page and basic UI elements. This serves as the starting point for the application's user interface.

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+function handleEnter() {
+  navigateTo('/about')
+}
+</script>
+
+<template>
+  <div class="flex justify-center pt-40">
+    <button @click="handleEnter" @keydown.enter="handleEnter" class="flex justify-center space-x-2 items-center uppercase focus:ring-4 focus-visible:outline-none focus:ring-black-12A focus:ring-offset-2 bg-orange-7 rounded-lg text-white px-4 py-2 font-thin text-2xl font-mono antialiased">
+      <NuxtLink to="/about">My Projects</NuxtLink>
+      <Icon name="uil:github" class="color-black" />
+    </button>
+  </div>
+  <div class="bg-black-12A text-white-12A p-10 mt-10 text-white text-2xl antialiased">
+    Hello!
+  </div>
+  <button class="bg-black">
+    Nothing work (as expected)
+  </button>
+</template>
+
+<style scoped>
+
+</style>
