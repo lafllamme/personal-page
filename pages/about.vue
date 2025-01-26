bfda79c235afd47a0ade19447aec53a052370dd8
Flame <teke900@gmail.com>
2025-01-27 00:01:45 +0100

    feat: Update About page layout and integrate PopOver components
    Revised the About page to include a new gradient background and improved spacing for better visual appeal. Added PopOver and PopOverOld components to enhance functionality and interactivity.

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
-
+import PopOver from '@/components/ui/PopOver/PopOver.vue'
+import PopOverOld from '@/components/ui/PopOver/PopOverOld.vue'
 </script>
 
 <template>
-  <section class="h-screen bg-blue-7">
-    <h1>This is about me</h1>
+  <section class="space-4 h-screen flex flex-col items-center justify-start from-teal9 to-green9 bg-gradient-to-br p-4 space-y-4">
+    <PopOver />
+    <PopOverOld />
   </section>
 </template>
 
