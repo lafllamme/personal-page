65225d189e08b08e54559f763406b553b3ec4ab5
Flame <teke900@gmail.com>
2025-01-28 22:45:17 +0100

    feat: Add MenuListItem component for navigation menu
    This new component provides a customizable menu list item using Radix Vue's NavigationMenuLink. It supports dynamic titles and slot content, with accessible styling and focus/hover interactions.

---
+++ components/ui/Navigation/Menu/MenuListItem.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { NavigationMenuLink } from 'radix-vue'
+
+defineProps({
+  title: String,
+})
+</script>
+
+<template>
+  <li>
+    <NavigationMenuLink as-child>
+      <a
+        class="block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-green7"
+        v-bind="$attrs"
+      >
+        <div class="mb-[5px] text-green12 font-medium leading-[1.2]">
+          {{ title }}
+        </div>
+        <p class="my-0 text-mauve11 leading-[1.4]">
+          <slot />
+        </p>
+      </a>
+    </NavigationMenuLink>
+  </li>
+</template>
