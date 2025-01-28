2f3fb7d305a674784e800fc28001b86f39b474c9
Flame <teke900@gmail.com>
2025-01-28 22:46:13 +0100

    feat: Replace PopOverOld with Navigation in About page
    Removed the outdated PopOverOld component and introduced the Navigation component for improved UI consistency. This change simplifies the structure and aligns the page with updated design standards.

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
+import Navigation from '@/components/ui/Navigation/Navigation.vue'
 import PopOver from '@/components/ui/PopOver/PopOver.vue'
-import PopOverOld from '@/components/ui/PopOver/PopOverOld.vue'
 </script>
 
 <template>
   <section class="space-4 h-screen flex flex-col items-center justify-start from-teal9 to-green9 bg-gradient-to-br p-4 space-y-4">
     <PopOver />
-    <PopOverOld />
+    <Navigation />
   </section>
 </template>
 
