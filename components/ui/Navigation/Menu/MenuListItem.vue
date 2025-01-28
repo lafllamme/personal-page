3e7ce27c1146c71c527aa5df7d8c0959b56d8585
Flame <teke900@gmail.com>
2025-01-28 22:53:26 +0100

    feat: refactor MenuListItem to use `useClsx` for class handling
    Replaced static class strings with dynamic `useClsx` bindings to simplify class management and improve readability. This change enhances maintainability and ensures consistency across class definitions.

--- components/ui/Navigation/Menu/MenuListItem.vue
+++ components/ui/Navigation/Menu/MenuListItem.vue
@@ -10,13 +10,17 @@ defineProps({
   <li>
     <NavigationMenuLink as-child>
       <a
-        class="block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-green7"
+        :class="useClsx(
+          'block select-none rounded-[6px] p-3 outline-none transition-colors',
+          'text-[15px] leading-none no-underline',
+          'hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-green7',
+        )"
         v-bind="$attrs"
       >
-        <div class="mb-[5px] text-green12 font-medium leading-[1.2]">
+        <div :class="useClsx('mb-[5px] text-green12 font-medium leading-[1.2]')">
           {{ title }}
         </div>
-        <p class="my-0 text-mauve11 leading-[1.4]">
+        <p :class="useClsx('my-0 text-mauve11 leading-[1.4]')">
           <slot />
         </p>
       </a>
