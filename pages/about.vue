7b3a9f2026d579a9df5231a260aab20b165d3bd6
Lafllamme <teke900@gmail.com>
2025-02-05 00:43:43 +0100

    feat: Replace components and update about page layout
    Replaced Navigation and PopOver components with a Card component. Also updated the section styling to use a new background class for a refreshed design.

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,10 @@
 <script lang="ts" setup>
-import Navigation from '@/components/ui/Navigation/Navigation.vue'
-import PopOver from '@/components/ui/PopOver/PopOver.vue'
+import Card from '@/components/ui/Card/Card.vue'
 </script>
 
 <template>
-  <section class="space-4 h-screen flex flex-col items-center justify-start from-teal9 to-green9 bg-gradient-to-br p-4 space-y-4">
-    <PopOver />
-    <Navigation />
+  <section class="space-4 h-screen flex flex-col items-center justify-start bg-black-12A p-4 space-y-4">
+    <Card />
   </section>
 </template>
 
