201a54b196bda88192f2d3b0fae380dd1d24d63e
Lafllamme <teke900@gmail.com>
2025-02-09 03:02:14 +0100

    feat: add dark mode styling to about page section
    Updated the about page section to support dark mode by adding a `dark:bg-black-12A` class. This ensures better visual consistency and user experience in dark mode.

--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,7 @@ import Card from '@/components/ui/Card/Card.vue'
 </script>
 
 <template>
-  <section class="space-4 h-screen flex flex-col items-center justify-start bg-black-12A p-4 space-y-4">
+  <section class="space-4 h-screen flex flex-col items-center justify-start bg-gray-2 p-4 space-y-4 dark:bg-black-12A">
     <Card />
   </section>
 </template>
