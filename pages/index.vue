13fa15224a506066f2535a293a021dd7f7d37dab
Flame <teke900@gmail.com>
2025-02-09 19:39:30 +0100

    feat: Refactor layout and improve button styling.
    Simplified and centered the page layout with full-screen height. Adjusted button text colors for better theme compatibility and readability. Removed redundant "Hello!" text block for a cleaner interface.

--- pages/index.vue
+++ pages/index.vue
@@ -7,27 +7,25 @@ function handleEnter() {
 </script>
 
 <template>
-  <div class="flex justify-center pt-40">
-    <button
-      :class="useClsx(
-        'flex items-center justify-center',
-        'rounded-lg bg-orange-7 px-4 py-2 text-2xl',
-        'text-white font-thin font-mono uppercase antialiased space-x-2',
-        'focus-visible:outline-none focus:ring-4 focus:ring-offset-2 focus:ring-black-12A',
-        'transition-all duration-900 ease-in-out',
-      )"
-      @click="handleEnter"
-      @keydown.enter="handleEnter"
-    >
-      <NuxtLink to="/about">
-        My Projects
-      </NuxtLink>
-      <Icon class="color-black" name="uil:github" />
-    </button>
-  </div>
-  <div
-    :class="useClsx('color-orange-11 text-2xl antialiased mt-10 p-10')"
-  >
-    Hello!
+  <div class="h-screen flex items-center justify-center">
+    <div>
+      <button
+        :class="useClsx(
+          'flex items-center justify-center',
+          'rounded-lg bg-orange-7 px-4 py-2 text-2xl',
+          'text-pureBlack dark:text-pureWhite',
+          'font-thin font-mono uppercase antialiased space-x-2',
+          'focus-visible:outline-none focus:ring-4 focus:ring-offset-2 focus:ring-black-12A',
+          'transition-all duration-900 ease-in-out',
+        )"
+        @click="handleEnter"
+        @keydown.enter="handleEnter"
+      >
+        <NuxtLink to="/about">
+          My Projects
+        </NuxtLink>
+        <Icon class="color-black" name="uil:github" />
+      </button>
+    </div>
   </div>
 </template>
