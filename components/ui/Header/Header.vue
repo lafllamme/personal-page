288461611a305e0bf12a5c562d06a7d93dbb4b33
Lafllamme <teke900@gmail.com>
2025-02-09 03:01:38 +0100

    feat: Add Header component with ColorMode integration
    Introduce a new Header component that includes the ColorMode component for theme switching. The Header is styled with a responsive layout and smooth transition effects, enhancing the user interface.

---
+++ components/ui/Header/Header.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ColorMode from '@/components/ui/ColorMode/ColorMode.vue'
+</script>
+
+<template>
+  <div class="flex justify-end bg-gray-2 p-4 duration-900 ease-[cubic-bezier(0.77,0,0.18,1)] dark:bg-black-12A">
+    <ColorMode />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
