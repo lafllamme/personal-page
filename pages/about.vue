3a48628bbe51bf306b4ed1c4c2cc25d529f7d6c7
Flame <teke900@gmail.com>
2025-02-09 19:40:06 +0100

    feat: update layout of About page for improved alignment
    Simplified the layout by adjusting flex properties and adding a wrapper div for better vertical alignment. Removed unnecessary spacing and background classes to streamline styling.

--- pages/about.vue
+++ pages/about.vue
@@ -3,8 +3,10 @@ import Card from '@/components/ui/Card/Card.vue'
 </script>
 
 <template>
-  <section class="space-4 h-screen flex flex-col items-center justify-start bg-gray-2 p-4 space-y-4 dark:bg-black-12A">
-    <Card />
+  <section class="h-screen flex justify-center">
+    <div class="pt-4">
+      <Card />
+    </div>
   </section>
 </template>
 
