5e392e7f78aca8aebdf1e3741d0709f4973d5806
Lafllamme <teke900@gmail.com>
2025-02-09 03:01:14 +0100

    feat: Add transition effect to button and update styling.
    Applied a smooth transition effect to the button for better UI responsiveness. Simplified and refined styles by removing redundant classes and unnecessary elements.Add color mode module and configuration to Nuxt
    
    Integrates the '@nuxtjs/color-mode' module to support dark and light themes. Configured default preferences, fallback options, and storage handling to enhance user experience.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,12 +4,24 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
-    '@nuxt/image', 
-    '@nuxt/icon', 
-    '@unocss/nuxt', 
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@unocss/nuxt',
     'radix-vue/nuxt',
-    '@nuxthub/core'
+    '@nuxthub/core',
+    '@nuxtjs/color-mode',
   ],
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'dark', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '-theme',
+    storage: 'localStorage', // or 'sessionStorage' or 'cookie'
+    storageKey: 'color-mode',
+  },
   css: [
     '@/assets/reset/main.ts',
   ],
