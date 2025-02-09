9b6786debfecea2eeecf2c19fc218da9a7b4fb3f
Flame <teke900@gmail.com>
2025-02-09 20:46:27 +0100

    feat: update background color for improved UI contrast
    Changed background color from gray-2 to gray-3 in the default layout to enhance visual contrast. Adjusted both template and styles to ensure consistency across light mode.

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@ import Header from '@/components/ui/Header/Header.vue'
 </script>
 
 <template>
-  <div class="scroll-none overflow-y-auto bg-gray-2 transition-colors duration-900 ease-[cubic-bezier(0.77,0,0.18,1)] dark:bg-black-12A">
+  <div class="scroll-none overflow-y-auto bg-gray-3 transition-colors duration-900 ease-[cubic-bezier(0.77,0,0.18,1)] dark:bg-black-12A">
     <!-- Shared Header (which may include your ColorMode component) -->
     <Header />
 
@@ -36,7 +36,7 @@ html {
 
 @media (prefers-color-scheme: light) {
   html {
-    @apply bg-gray-2;
+    @apply bg-gray-3;
   }
 }
 </style>
