08e0ec2ad7cbcec99b49473f0dabf21415f196ce
Flame <teke900@gmail.com>
2025-02-09 19:39:59 +0100

    feat: improve loading indicator and page transitions
    Enhanced the loading indicator by adding a gradient color for better visuals. Adjusted page transitions to use smoother opacity and blur effects, improving the overall user experience.

--- app.vue
+++ app.vue
@@ -1,9 +1,40 @@
 <script lang="ts" setup>
+const loadingGradient = ref('repeating-linear-gradient(to right, #8a2387, #e94057, #f27121)')
 </script>
 
 <template>
   <NuxtLayout>
-    <NuxtLoadingIndicator :throttle="0" />
+    <NuxtLoadingIndicator
+      :color="loadingGradient"
+      :throttle="0"
+    />
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style lang="scss">
+// page transitions
+/* Enhanced soft fade transition without scaling to avoid jumpiness */
+.page-enter-active,
+.page-leave-active {
+  @apply duration-300 ease-in-out !important;
+  transition-property: opacity, filter !important;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(2px); /* Start with a light blur */
+}
+
+.page-enter-to {
+  opacity: 1;
+  transition-delay: 0.1s; /* Delay the clearing of the blur for a smooth transition */
+  filter: blur(0);
+}
+
+.page-leave-active {
+  opacity: 0;
+  filter: blur(4px); /* Slightly more blur on leave for a soft exit */
+}
+</style>
