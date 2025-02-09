31b3dbc4e4a9872d1ec662ba4d1e07b19b54b577
Flame <teke900@gmail.com>
2025-02-09 19:39:47 +0100

    feat: Refactor layout structure and improve scrollbar handling.
    Wrapped the layout in a new div with styling for smooth transitions and dark mode. Updated slots for better content organization and added styles to hide scrollbars across browsers. Enhanced scroll behavior and streamlined theme integration.

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,33 +3,34 @@ import Header from '@/components/ui/Header/Header.vue'
 </script>
 
 <template>
-  <!-- Shared Header (which may include your ColorMode component) -->
-  <Header />
+  <div class="scroll-none overflow-y-auto bg-gray-2 transition-colors duration-900 ease-[cubic-bezier(0.77,0,0.18,1)] dark:bg-black-12A">
+    <!-- Shared Header (which may include your ColorMode component) -->
+    <Header />
 
-  <!-- Body wrapper for page-specific content -->
-  <main class="bg-gray-2 transition-colors duration-900 ease-[cubic-bezier(0.77,0,0.18,1)] dark:bg-black-12A">
-    <slot />
-  </main>
+    <!-- Body wrapper for page-specific content -->
+    <main>
+      <slot name="default" />
+    </main>
 
-  <!-- Shared Footer (if needed) -->
-<!--  <Footer /> -->
+    <!-- Shared Footer (if needed) -->
+    <!--  <Footer /> -->
+  </div>
 </template>
 
 <style lang="scss">
-//TODO: Fix dark mode settings
+html::-webkit-scrollbar,
+body::-webkit-scrollbar {
+  display: none; // WebKit (Chrome, Safari, Edge)
+}
+
 html {
-  @apply transition-colors duration-900 ease-[cubic-bezier(0.77,0,0.18,1)];
-  &.dark-theme {
-    background: rgb(25, 25, 25);
-  }
-  &.light-theme {
-    @apply bg-gray-2;
-  }
+  scrollbar-width: none; // Firefox: Scrollbar ausblenden
+  scroll-behavior: smooth;
 }
 
 @media (prefers-color-scheme: dark) {
   html {
-    @apply bg-black-12A;
+    @apply rgba(25, 25, 25);
   }
 }
 
