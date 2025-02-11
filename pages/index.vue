d5b6b32200b572ebfacb95588c83bc8a614b3597
Flame <teke900@gmail.com>
2025-02-11 23:18:52 +0100

    feat: enhance button accessibility and visuals
    Added accessibility improvements by including `aria-label` and `tabindex` for better screen reader support. Updated button styles with a border and adjusted shadow properties for a more refined appearance.

--- pages/index.vue
+++ pages/index.vue
@@ -12,17 +12,18 @@ function handleEnter() {
       <button
         :class="useClsx(
           'flex items-center justify-center',
+          'border-pureBlack dark:border-pureWhite border-2',
           'rounded-lg bg-pureWhite dark:bg-pureBlack px-4 py-2 text-2xl',
           'text-mint-12',
           'font-thin font-mono uppercase antialiased space-x-2',
-          'shadow-[0px_8px_100px_0px] shadow-mint-7',
+          'shadow-[6px_8px_100px_6px] shadow-mint-7',
           'focus-visible:outline-none focus:ring-4 focus:ring-offset-2 focus:ring-black-12A',
           'transition-color duration-900 ease-in-out',
         )"
         @click="handleEnter"
         @keydown.enter="handleEnter"
       >
-        <NuxtLink to="/about">
+        <NuxtLink aria-label="My Projects" tabindex="-1" to="/about">
           My Projects
         </NuxtLink>
         <Icon class="color-black" name="uil:github" />
