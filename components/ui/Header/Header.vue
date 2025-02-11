89d60e45f63513cdebcd0d7ce3cc4d172aa0ce59
Flame <teke900@gmail.com>
2025-02-11 23:59:32 +0100

    feat: adjust Header color mode positioning for responsiveness
    Update the `ColorMode` component's class to use a responsive utility. This ensures better alignment on smaller screens and maintains proper positioning on larger ones.

--- components/ui/Header/Header.vue
+++ components/ui/Header/Header.vue
@@ -6,14 +6,19 @@ import ColorMode from '~/components/ui/ColorMode/ColorMode.vue'
 <template>
   <header role="banner">
     <nav aria-labelledby="mainMenuLabel">
-      <h2 id="mainMenuLabel" class="sr-only">
+      <h2
+        id="mainMenuLabel"
+        class="sr-only"
+      >
         Main Menu
       </h2>
       <div class="flex items-center">
         <!-- The actual site navigation -->
         <Navigation class="pt-2" />
         <!-- The color mode toggle, on the same row but not part of the nav-list -->
-        <ColorMode class="absolute right-4 top-4" />
+        <ColorMode
+          :class="useClsx('absolute right-2 top-4 md:right-4')"
+        />
       </div>
     </nav>
   </header>
