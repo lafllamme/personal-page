d7112b00fd2d69919a9e1be7e211d483a50a5b47
Flame <teke900@gmail.com>
2025-02-13 22:59:15 +0100

    feat: add mobile navigation component with toggle buttons
    Introduced a new `NavigationMobile.vue` component for the mobile navigation UI. It includes a color mode toggle and buttons for search and menu with toggle functionality. Initial styling and transitions are also handled.

---
+++ components/ui/Navigation/Mobile/NavigationMobile.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue'
+import ColorMode from '~/components/ui/ColorMode/ColorMode.vue'
+
+const isOpen = ref(false)
+function handleClick() {
+  console.log('clicked')
+  isOpen.value = !isOpen.value
+}
+</script>
+
+<template>
+  <div class="flex justify-center space-x-2">
+    <ColorMode />
+    <button>
+      <Icon
+        class="h-10 w-10 color-pureBlack transition-transform duration-700 ease-in-out dark:color-pureWhite"
+        icon="ri:search-2-line"
+        @click="handleClick"
+      />
+    </button>
+
+    <button>
+      <Icon
+        :class="isOpen ? 'rotate-180' : ''"
+        class="h-10 w-10 color-pureBlack transition-transform duration-700 ease-in-out dark:color-pureWhite"
+        icon="ri:menu-3-line"
+        @click="handleClick"
+      />
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
