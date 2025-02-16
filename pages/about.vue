9163c92136dcad58f89ab4639d08df9bb1429c94
Lafllamme <teke900@gmail.com>
2025-02-16 12:24:27 +0100

    feat: replace Background with Card in About page
    Replaced the Background component with the Card component on the About page. Adjusted styling to align with the new layout and structure. This improves the design and better suits the page's purpose.

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,9 @@
 <script lang="ts" setup>
-import Background from '@/components/ui/Background/Background.vue'
+import Card from '@/components/ui/Card/Card.vue'
 </script>
 
 <template>
-  <section class="h-screen flex justify-center">
-    <Background />
+  <section class="space-4 h-screen flex flex-col items-center justify-start p-4 space-y-4">
+    <Card />
   </section>
 </template>
-
-<style scoped>
-
-</style>
