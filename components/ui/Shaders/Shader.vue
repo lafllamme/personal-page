4a335b1486132add45457a308e64924cb019d7b6
Lafllamme <teke900@gmail.com>
2025-02-20 01:11:34 +0100

    feat: Add Shader.vue component with Three.js shader integration
    This component utilizes GLSL shaders for rendering with reactive uniforms. It dynamically syncs resolution with window size and implements a time-based animation loop. Includes a canvas setup using Tres.js and a plane mesh rendered with custom vertex and fragment shaders.

---
+++ components/ui/Shaders/Shader.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { useWindowSize } from '@vueuse/core'
+import { Vector2 } from 'three'
+import fragmentShader from './fragment.glsl?raw'
+import vertexShader from './vertex.glsl?raw'
+
+// Reactive window size
+const { width, height } = useWindowSize()
+
+// GLSL uniforms
+const uniforms = reactive({
+  iTime: { value: 0 },
+  iResolution: { value: new Vector2(width.value, height.value) },
+})
+
+const animationFrame = ref(0)
+
+// Animate iTime
+function animate(t: number) {
+  uniforms.iTime.value = t * 0.001
+  animationFrame.value = requestAnimationFrame(animate)
+}
+
+onMounted(() => {
+  animationFrame.value = requestAnimationFrame(animate)
+})
+
+onUnmounted(() => {
+  cancelAnimationFrame(animationFrame.value)
+})
+// Keep iResolution in sync with the window size
+watch([width, height], () => {
+  uniforms.iResolution.value.set(width.value, height.value)
+})
+</script>
+
+<template>
+  <!-- Make the canvas fill the entire screen -->
+  <TresCanvas
+    window-size
+    preset="realistic"
+  >
+    <TresPerspectiveCamera
+      visible
+      :position="[0, 0, 1]"
+      :args="[45, 1, 0.1, 1000]"
+    />
+    <OrbitControls />
+    <!-- A plane sized to the same width/height as the camera's boundaries -->
+    <TresMesh>
+      <TresPlaneGeometry :args="[width, height]" />
+      <TresShaderMaterial
+        :vertex-shader="vertexShader"
+        :fragment-shader="fragmentShader"
+        :uniforms="uniforms"
+      />
+    </TresMesh>
+  </TresCanvas>
+</template>
