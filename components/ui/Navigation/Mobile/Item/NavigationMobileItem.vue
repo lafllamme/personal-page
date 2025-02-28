ecc0855c84bff3445c9cf15799f7cf5a4c99a56a
Lafllamme <teke900@gmail.com>
2025-03-01 00:57:10 +0100

    feat: Add NavigationMobileItem component with defaults and typings
    Introduce `NavigationMobileItem` to handle mobile navigation items with props and default values. The component ensures proper typing using TypeScript and renders a flexible navigation list with accessibility enhancements like tabindex support.

---
+++ components/ui/Navigation/Mobile/Item/NavigationMobileItem.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import type { NavigationItemProps } from './NavigationMobileItem.model'
+import { NavigationItemDefaults } from './NavigationMobileItem.model'
+
+const props = withDefaults(defineProps<NavigationItemProps>(), NavigationItemDefaults)
+const { items, tabIndex } = toRefs(props)
+</script>
+
+<template>
+  <nav class="flex flex-col gap-4">
+    <NuxtLinkLocale
+      v-for="(item, idx) in items"
+      :key="`${item.label}-${idx}`"
+      :class="useClsx(
+        'hover:text-gray-600 dark:hover:text-gray-300 focus:ring-blue-500 rounded-sm',
+        'text-pureBlack transition-colors dark:text-pureWhite',
+        'focus:outline-none focus:ring-2 focus:ring-offset-2')"
+      :tabindex="tabIndex"
+      :to="item.href"
+    >
+      {{ item.label }}
+    </NuxtLinkLocale>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
