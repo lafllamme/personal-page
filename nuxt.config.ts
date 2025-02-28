e5ff58df334f5a8b7284a0b5c98c3fa0435c36e9
Lafllamme <teke900@gmail.com>
2025-02-28 14:01:56 +0100

    feat: Add i18n configuration and integration to Nuxt project
    Introduced multilingual support with i18n, defining multiple locales, default locale, and browser language detection. Updated the module list to include the @nuxtjs/i18n package and configured route prefixing and fallback behavior. This enhances the application's internationalization capabilities.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-// eslint-disable-next-line no-undef
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
   },
-
+  i18n: {
+    strategy: 'prefix', // add prefix to all generated routes
+    defaultLocale: 'en',
+    locales: [
+      { code: 'de', iso: 'de-DE', name: 'Deutsch' },
+      { code: 'en', iso: 'en-US', name: 'English' },
+      { code: 'fr', iso: 'fr-FR', name: 'Français' },
+      { code: 'ja', iso: 'ja-JP', name: '日本語' },
+      { code: 'zh', iso: 'zh-CN', name: '中文' },
+      { code: 'pt', iso: 'pt-PT', name: 'Português' },
+      { code: 'es', iso: 'es-ES', name: 'Español' },
+      { code: 'pl', iso: 'pl-PL', name: 'Polski' },
+      { code: 'da', iso: 'da-DK', name: 'Dansk' },
+      { code: 'cs', iso: 'cs-CZ', name: 'Čeština' },
+      { code: 'nl', iso: 'nl-NL', name: 'Nederlands' },
+      { code: 'el', iso: 'el-GR', name: 'Ελληνικά' },
+      { code: 'tr', iso: 'tr-TR', name: 'Türkçe' },
+      { code: 'ko', iso: 'ko-KR', name: '한국어' },
+      { code: 'uk', iso: 'uk-UA', name: 'Українська' },
+    ],
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      alwaysRedirect: true,
+      fallbackLocale: 'en',
+    },
+    vueI18n: './i18n.config.ts',
+  },
   tres: {
     devtools: true,
     glsl: true,
@@ -18,7 +45,9 @@ export default defineNuxtConfig({
     'radix-vue/nuxt',
     '@nuxthub/core',
     '@nuxtjs/color-mode',
+    '@nuxtjs/i18n',
     '@tresjs/nuxt',
+    '@nuxtjs/i18n',
   ],
   colorMode: {
     preference: 'system', // default value of $colorMode.preference
