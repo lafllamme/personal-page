cd614800154db157514b562a6ca7bcac58071add
Lafllamme <teke900@gmail.com>
2025-03-07 02:54:45 +0100

    feat: optimize shader rendering and improve responsiveness
    Added debounced resolution updates and document visibility checks to reduce redundant calculations and improve performance. Introduced conditional camera selection based on screen size for better responsiveness. Enhanced shader selection logic for dynamic adaptation to dark mode and breakpoints.

--- components/ui/Shaders/Shader.vue
+++ components/ui/Shaders/Shader.vue
@@ -1,16 +1,18 @@
 <script lang="ts" setup>
-import { breakpointsTailwind, useBreakpoints, useWindowSize } from '@vueuse/core'
+import { breakpointsTailwind, useBreakpoints, useDebounceFn, useDocumentVisibility, useWindowSize } from '@vueuse/core'
 import { Vector3 } from 'three'
 import fragmentDesktopDark from './Dark/fragment.glsl?raw'
 import fragmentMobileDark from './Dark/fragmentMobile.glsl?raw'
 import fragmentDesktop from './Light/fragment.glsl?raw'
 import fragmentMobile from './Light/fragmentMobile.glsl?raw'
-
 import vertexShader from './vertex.glsl?raw'
 
+// Color mode and breakpoints
 const colorMode = useColorMode()
 const isDark = computed(() => colorMode.value === 'dark')
 const breakpoints = useBreakpoints(breakpointsTailwind)
+
+// Choose the shader based on breakpoints and dark mode
 const shader = computed(() => {
   if (breakpoints.smaller('md').value) {
     return isDark.value ? fragmentMobileDark : fragmentMobile
@@ -28,18 +30,25 @@ const uniforms = {
   iResolution: { value: new Vector3(width.value, height.value, 1) },
 }
 
+// Use document visibility to pause the render loop when the tab is inactive
+const isVisible = useDocumentVisibility()
+
 const { onLoop } = useRenderLoop()
 
-onLoop(({ delta, elapsed }) => {
+onLoop(({ elapsed }) => {
+  if (!isVisible.value)
+    return
   if (shaderRef.value) {
-    /* console.log('elapsed', elapsed)
-    console.log('delta', delta) */
     shaderRef.value.uniforms.iTime.value = elapsed
   }
 })
-watch([width, height], () => {
+
+// Debounced watcher for resolution updates to prevent too many recalculations on resize
+const updateResolution = useDebounceFn(() => {
   uniforms.iResolution.value.set(width.value, height.value, 1)
-})
+}, 0)
+
+watch([width, height], updateResolution)
 </script>
 
 <template>
