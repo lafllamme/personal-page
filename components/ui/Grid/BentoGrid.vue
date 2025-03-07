2f1c2b1ca0e5f26c7f1f30acf6ed237cb41c15ba
Lafllamme <teke900@gmail.com>
2025-03-07 02:12:37 +0100

    feat: Add Grid, GridItem, and GridCard components
    Introduce reusable Grid, GridItem, and GridCard components for building flexible and responsive layouts. These components leverage modern CSS utilities and Vue slots to allow for highly customizable and maintainable UI design.

---
+++ components/ui/Grid/BentoGrid.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from 'vue'
+
+interface Props {
+  class?: HTMLAttributes['class']
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div
+    :class="
+      useClsx(
+        'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 grid-flow-dense md:auto-rows-[18rem] gap-4 max-w-full py-6 px-12 mx-auto',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </div>
+</template>
---
+++ components/ui/Grid/GridItem/GridItem.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from 'vue'
+
+interface Props {
+  class?: HTMLAttributes['class']
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div
+    :class="
+      useClsx(
+        'justify-between flex flex-col space-y-4 row-span-1 rounded-xl',
+        'transition-colors duration-900 ease-[cubic-bezier(0.77,0,0.18,1)]',
+        'group/bento  transition duration-200  border border-transparent',
+        'dark:shadow-none p-4 bg-pureWhite dark:bg-pureBlack dark:border-pureWhite/[0.2]',
+        'focus:outline-none focus:ring-4  focus:ring-pureBlack dark:focus:ring-pureWhite',
+        'hover:shadow-[rgba(0,0,0,0.19)_0px_10px_20px,rgba(0,0,0,0.23)_0px_6px_6px] hover:dark:shadow-[rgba(255,255,255,0.15)_0px_10px_20px,rgba(255,255,255,0.2)_0px_6px_6px]',
+        props.class,
+      )
+    "
+    tabindex="0"
+  >
+    <slot name="header" />
+    <div class="transition duration-200 group-hover/bento:translate-x-2">
+      <slot name="icon" />
+      <div class="my-2 color-pureBlack font-bold font-sans dark:color-pureWhite">
+        <slot name="title" />
+      </div>
+      <div class="text-xs color-pureBlack font-normal font-sans dark:color-pureWhite">
+        <slot name="description" />
+      </div>
+    </div>
+  </div>
+</template>
