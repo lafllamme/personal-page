efb5fcec57a1780d5e11b7ff815db0f4f5a039ff
Lafllamme <teke900@gmail.com>
2025-03-07 01:11:36 +0100

    chore: Add BentoGrid component to display list of items
    Introduced the BentoGrid and BentoGridItem components to structure and showcase a list of predefined items with titles and descriptions. This update enhances the visual layout and organization of the page content.

--- pages/test.vue
+++ pages/test.vue
@@ -1,9 +1,69 @@
 <script lang="ts" setup>
+import BentoGrid from '@/components/ui/Grid/BentoGrid.vue'
+import BentoGridItem from '@/components/ui/Grid/GridItem/GridItem.vue'
+import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
+
+const breakpoints = useBreakpoints(breakpointsTailwind)
+
+const items = [
+  {
+    title: 'The Dawn of Innovation',
+    description: 'Explore the birth of groundbreaking ideas and inventions.',
+  },
+  {
+    title: 'The Digital Revolution',
+    description: 'Dive into the transformative power of technology.',
+  },
+  {
+    title: 'The Art of Design',
+    description: 'Discover the beauty of thoughtful and experience design.',
+  },
+  {
+    title: 'The Power of Communication',
+    description: 'Understand the impact of effective communication in our lives.',
+  },
+  {
+    title: 'The Pursuit of Knowledge',
+    description: 'Join the quest for understanding and enlightenment.',
+  },
+  {
+    title: 'The Joy of Creation',
+    description: 'Experience the thrill of bringing ideas to life.',
+  },
+  {
+    title: 'The Spirit of Adventure',
+    description: 'Embark on exciting journeys and thrilling discoveries.',
+  },
+]
 </script>
 
 <template>
   <!-- Sample page content for testing neon styles -->
   <div>
+    <BentoGrid class="mx-auto max-w-7xl">
+      <BentoGridItem
+        v-for="(item, index) in items"
+        :key="index"
+        :class="index === 3 || index === 6 ? 'md:col-span-2' : ''"
+      >
+        <template #header>
+          <div class="size-full flex animate-pulse space-x-4">
+            <div class="size-full flex flex-1 rounded-md bg-slate-7" />
+          </div>
+        </template>
+
+        <template #title>
+          <strong>{{ item.title }}</strong>
+        </template>
+
+        <template #icon />
+
+        <template #description>
+          <p>{{ item.description }}</p>
+        </template>
+      </BentoGridItem>
+    </BentoGrid>
+
     <main id="main-content" class="pt-32 space-y-16">
       <section class="bg-black text-pink-500 min-h-screen flex flex-col items-center justify-center">
         <h1 class="text-5xl font-bold drop-shadow-[0_0_10px_#ff00ff]">
