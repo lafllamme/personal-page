baec86eb2e35df3e4169db987207d5696339169b
Lafllamme <teke900@gmail.com>
2025-03-12 02:59:35 +0100

    feat: enhance MobileMenuButton accessibility and animations
    Added ARIA attributes for better accessibility and updated styles for smoother transitions and animations. Refined class bindings to improve UX, ensuring consistent visual feedback and interaction.

--- components/ui/Navigation/Mobile/MobileMenuButton.vue
+++ components/ui/Navigation/Mobile/MobileMenuButton.vue
@@ -7,12 +7,22 @@ defineEmits({
 
 <template>
   <button
+    :aria-expanded="isOpen"
+    :class="useClsx(
+      '',
+      'flex items-center justify-center duration-200',
+      'hover:scale-110 transition-transform focus:outline-none',
+      'color-pureBlack dark:color-pureWhite',
+      'focus:ring-pureBlack dark:focus:ring-pureWhite focus:ring',
+    )"
+    aria-controls="mobile-menu"
+    aria-haspopup="true"
     aria-label="Toggle menu"
-    class="text-gray-700 hover:text-gray-900 flex items-center justify-center p-2 transition-all duration-200 hover:scale-110 focus:outline-none"
     @click="$emit('toggle')"
   >
     <svg
-      class="transform transition-transform duration-300"
+      :class="isOpen && '-translate-x-2'"
+      class="h-10 w-10 transform transition-transform duration-300"
       height="24"
       viewBox="0 0 24 24"
       width="24"
@@ -20,20 +30,21 @@ defineEmits({
     >
       <!-- Top stripe -->
       <path
-        :class="[isOpen ? 'translate-y-[7px] rotate-45' : '']"
+        :class="[isOpen ? 'translate-y-[5px] rotate-45' : '']"
         class="origin-center transform transition-all duration-300"
         d="M3 4h18v2H3z"
         fill="currentColor"
       />
       <!-- Middle stripe -->
       <path
-        :class="[isOpen ? 'opacity-0 translate-x-3' : '']" class="transform transition-all duration-300"
+        :class="[isOpen ? 'opacity-0 translate-x-3' : '']"
+        class="transform transition-all duration-300"
         d="M9 11h12v2H9z"
         fill="currentColor"
       />
       <!-- Bottom stripe -->
       <path
-        :class="[isOpen ? 'translate-y-[-7px] -rotate-45' : '']"
+        :class="[isOpen ? 'translate-y-[-5px] -rotate-45' : '']"
         class="origin-center transform transition-all duration-300"
         d="M3 18h18v2H3z"
         fill="currentColor"
