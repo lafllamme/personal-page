06a70c8ac2df56a53819fac4975e3ddc3121879f
Flame <teke900@gmail.com>
2025-03-28 22:32:13 +0100

    feat: add Underline component with props and default values for styling
    This ensures we have a hover underline for specific areas.

---
+++ components/ui/Menu/Underline/Underline.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import type { UnderlineProps } from '@/components/ui/Menu/Underline/Underline.model'
+import { UnderlinePropsDefaults } from '@/components/ui/Menu/Underline/Underline.model'
+
+const props = withDefaults(defineProps<UnderlineProps>(), UnderlinePropsDefaults)
+const { className, color } = props
+</script>
+
+<template>
+  <!-- Hover indicator -->
+  <span
+    :class="useClsx(
+      className,
+      color,
+      'absolute bottom-0 left-1/2 h-[1px]',
+      'w-0 transform transition-all duration-300 media-mouse:group-hover:w-3/4 -translate-x-1/2',
+    )"
+  />
+</template>
