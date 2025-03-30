bd324d4a5f32d39f11f0dd7ac7737bdbe36f9911
Flame <teke900@gmail.com>
2025-03-30 16:57:27 +0200

    feat: add MenuButton component with props and default values for toggle functionality
    Adds Menu Button back as component with model file.

---
+++ components/ui/Menu/Button/MenuButton.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+import type { MenuButtonEmits, MenuButtonProps } from '@/components/ui/Menu/Button/MenuButton.model'
+import { MenuButtonDefaults } from '@/components/ui/Menu/Button/MenuButton.model'
+
+withDefaults(defineProps<MenuButtonProps>(), MenuButtonDefaults)
+defineEmits<MenuButtonEmits>()
+</script>
+
+<template>
+  <button
+    :aria-expanded="isOpen"
+    :class="useClsx(
+      '',
+      'flex items-center justify-center',
+      'hover:scale-105 transition-transform focus:outline-none ease-in-out duration-300',
+      'color-pureBlack dark:color-pureWhite',
+      'focus:ring-pureBlack dark:focus:ring-pureWhite focus:ring',
+    )"
+    aria-controls="mobile-menu"
+    aria-haspopup="true"
+    aria-label="Toggle menu"
+    @click="$emit('toggle')"
+  >
+    <svg
+      :class="isOpen && '-translate-x-2'"
+      class="h-10 w-10 transform transition-transform duration-200 ease-in-out"
+      height="24"
+      viewBox="0 0 24 24"
+      width="24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <!-- Top stripe -->
+      <path
+        :class="[isOpen ? 'translate-y-[5px] rotate-45' : '']"
+        class="origin-center transform transition-all duration-300"
+        d="M3 4h18v2H3z"
+        fill="currentColor"
+      />
+      <!-- Middle stripe -->
+      <path
+        :class="[isOpen ? 'opacity-0 translate-x-3' : '']"
+        class="transform transition-all duration-300"
+        d="M9 11h12v2H9z"
+        fill="currentColor"
+      />
+      <!-- Bottom stripe -->
+      <path
+        :class="[isOpen ? 'translate-y-[-5px] -rotate-45' : '']"
+        class="origin-center transform transition-all duration-300"
+        d="M3 18h18v2H3z"
+        fill="currentColor"
+      />
+    </svg>
+  </button>
+</template>
