ad2849fa8a8939807f98240a9ee8a62aa96f4000
Flame <teke900@gmail.com>
2025-04-03 23:23:48 +0200

    feat: add global app configuration for meta tags

---
+++ app.config.ts
@@ -0,0 +1,7 @@
+// app.config.ts
+export default defineAppConfig({
+  // Custom config for global head meta tags
+  meta: {
+    viewport: 'width=device-width, initial-scale=1, viewport-fit=cover',
+  },
+})
