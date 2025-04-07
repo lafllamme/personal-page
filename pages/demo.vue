021a769f5336928a618fbf8b56dcb31b55f261f8
Lafllamme <teke900@gmail.com>
2025-04-07 23:42:22 +0200

    feat: add demo.vue with interactive elements for user engagement

---
+++ pages/demo.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="cursor-button">
+    Click Me
+  </button>
+  <a class="cursor-link" href="#">Read More</a>
+  <div class="cursor-image">
+    <img alt="Description" src="https://placehold.co/600x400">
+  </div>
+  <h1 class="cursor-heading">
+    Main Heading
+  </h1>
+  <span class="cursor-highlight">Important text</span>
+</template>
