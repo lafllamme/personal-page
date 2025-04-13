9cdb29b9c3c24a97361ec9ce954d8e6b27db3b7a
Lafllamme <teke900@gmail.com>
2025-04-13 21:47:51 +0200

    feat: enhance Underline component with variant support for flexible positioning

--- components/ui/Menu/Underline/Underline.vue
+++ components/ui/Menu/Underline/Underline.vue
@@ -3,17 +3,22 @@ import type { UnderlineProps } from '@/components/ui/Menu/Underline/Underline.mo
 import { UnderlinePropsDefaults } from '@/components/ui/Menu/Underline/Underline.model'
 
 const props = withDefaults(defineProps<UnderlineProps>(), UnderlinePropsDefaults)
-const { className, color } = props
+const { className, color, variant } = props
+
+const underlineClasses = computed(() => {
+  return useClsx(
+    color || 'bg-mint-8',
+    'absolute bottom-0 h-[2px] w-0 transform transition-all duration-300 media-mouse:group-hover:w-3/4',
+    // Conditionally set starting position and transform based on variant
+    variant === 'left'
+      ? 'left-0' // For "left", position at left without centering transform.
+      : 'left-1/2 -translate-x-1/2', // Default centered variant.
+    className,
+  )
+})
 </script>
 
 <template>
-  <!-- Hover indicator -->
-  <span
-    :class="useClsx(
-      color ? color : 'bg-mint-8',
-      'absolute bottom-0 left-1/2 h-[2px]',
-      'w-0 transform transition-all duration-300 media-mouse:group-hover:w-3/4 -translate-x-1/2',
-      className,
-    )"
-  />
+  <!-- Underline span -->
+  <span :class="underlineClasses" />
 </template>
