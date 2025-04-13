919b011e109eab300bdac263c879aed176a05a94
Lafllamme <teke900@gmail.com>
2025-04-13 21:46:50 +0200

    feat: add fade-in animation to Cursor component for improved visual effect

--- components/ui/Menu/Underline/Underline.model.ts
+++ components/ui/Menu/Underline/Underline.model.ts
@@ -1,9 +1,13 @@
+export type variant = 'center' | 'left' | 'right'
+
 export interface UnderlineProps {
   className?: string
+  variant?: variant
   color?: string
 }
 
 export const UnderlinePropsDefaults: Partial<UnderlineProps> = {
   className: '',
+  variant: 'center',
   color: 'bg-mint-8 ',
 }
