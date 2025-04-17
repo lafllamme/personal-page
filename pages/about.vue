c52491f0c75f9fbdd89d87aec24b151d3ba8b600
Lafllamme <teke900@gmail.com>
2025-04-18 01:25:27 +0200

    refactor: adjust padding in about section and remove unused PopOver component

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,9 @@
 <script lang="ts" setup>
 import Card from '@/components/ui/Card/Card.vue'
-import PopOver from '@/components/ui/PopOver/PopOver.vue'
 </script>
 
 <template>
-  <section class="space-4 flex flex-col items-center justify-start p-4 space-y-4">
+  <section class="space-4 flex flex-col items-center justify-start p-4 pt-20 space-y-4">
     <Card />
-    <PopOver />
   </section>
 </template>
