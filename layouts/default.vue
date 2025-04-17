d1da13b4dbf9f125945e6bda85296b69f2d520ba
Lafllamme <teke900@gmail.com>
2025-04-18 01:25:20 +0200

    refactor: simplify main element by removing unnecessary padding

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,7 +15,7 @@ import Shader from '@/components/ui/Shaders/Shader.vue'
       class="relative z-10 overflow-y-auto transition-colors duration-900 ease-[cubic-bezier(0.77,0,0.18,1)]"
     >
       <Header />
-      <main class="pt-20">
+      <main>
         <slot name="default" />
       </main>
     </div>
