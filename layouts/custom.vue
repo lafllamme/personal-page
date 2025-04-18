f740a0755368732dc1f9ef1e20aef7bb39b9a246
Lafllamme <teke900@gmail.com>
2025-04-19 00:48:23 +0200

    feat: create custom.vue component with Header integration and layout structure

---
+++ layouts/custom.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Header from '@/components/ui/Header/Header.vue'
+</script>
+
+<template>
+  <div class="relative overflow-hidden bg-gray-3 dark:bg-gray-2">
+    <!-- Main Content -->
+    <div
+      class="relative z-10 overflow-y-auto transition-colors duration-900 ease-[cubic-bezier(0.77,0,0.18,1)]"
+    >
+      <Header />
+      <main>
+        <slot name="default" />
+      </main>
+    </div>
+  </div>
+</template>
