8f54951f50b6afe7c8020780ef681318b4b75f37
Lafllamme <teke900@gmail.com>
2025-04-18 17:44:15 +0200

    feat: add ReviewCard model and default props

---
+++ components/ui/ReviewCard/ReviewCard.model.ts
@@ -0,0 +1,17 @@
+export interface ReviewCardProps {
+  index: number
+  max: number
+  img: string
+  name: string
+  username: string
+  body: string
+}
+
+export const ReviewCardDefaultProps: ReviewCardProps = {
+  index: 0,
+  max: 3,
+  img: 'https://i.pravatar.cc/150?img=3',
+  name: 'John Doe',
+  username: 'johndoe',
+  body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel sapien lectus. Sed at nunc non nisi efficitur bibendum. Nullam ut ligula nec erat tincidunt varius.',
+}
