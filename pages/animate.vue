9a1e90238ce75e4b671ff1e36ab6b12cc30a847f
Lafllamme <teke900@gmail.com>
2025-04-18 18:23:52 +0200

    feat: add animate.vue for displaying animated content with Spline component

---
+++ pages/animate.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import Spline from '@/components/ui/Background/Spline/Spline.vue'
+
+const sceneUrl = 'https://prod.spline.design/kZDDjO5HuC9GJUM2/scene.splinecode'
+</script>
+
+<template>
+  <div class="relative w-full overflow-hidden rounded-lg bg-pureWhite/96 pt-20 dark:bg-pureBlack/96">
+    <div
+      class="font-heading text-pureblack absolute w-full flex flex-col items-center justify-center gap-2 p-8 text-center dark:text-pureWhite"
+    >
+      <span class="text-4xl font-semibold"> TecNew </span>
+      <span class="font-light font-sans">Read about latest news in animations with style.</span>
+    </div>
+    <div class="flex">
+      <Spline
+        :scene="sceneUrl"
+        class="mt-24 size-full"
+      />
+    </div>
+  </div>
+</template>
