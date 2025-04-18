a263efc7ecc64639ca9297925a933f8c95daf180
Lafllamme <teke900@gmail.com>
2025-04-19 00:48:30 +0200

    feat: update animate.vue with custom layout and SEO metadata

--- pages/animate.vue
+++ pages/animate.vue
@@ -2,6 +2,20 @@
 import Spline from '@/components/ui/Background/Spline/Spline.vue'
 
 const sceneUrl = 'https://prod.spline.design/kZDDjO5HuC9GJUM2/scene.splinecode'
+definePageMeta({
+  layout: 'custom',
+  title: 'TecNews - Animation News',
+  meta: [
+    {
+      name: 'description',
+      content: 'Read about latest news in animations with style.',
+    },
+    {
+      name: 'keywords',
+      content: 'animation, news, latest, style, TecNews',
+    },
+  ],
+})
 </script>
 
 <template>
@@ -9,8 +23,8 @@ const sceneUrl = 'https://prod.spline.design/kZDDjO5HuC9GJUM2/scene.splinecode'
     <div
       class="font-heading text-pureblack absolute w-full flex flex-col items-center justify-center gap-2 p-8 text-center dark:text-pureWhite"
     >
-      <span class="text-4xl font-semibold"> TecNew </span>
-      <span class="font-light font-sans">Read about latest news in animations with style.</span>
+      <span class="geist-regular text-4xl !font-bold">TecNews</span>
+      <span class="font-light font-mono font-sans">Read about latest news in animations with style.</span>
     </div>
     <div class="flex">
       <Spline
