1304cd5297e12d45dc79ab12c1cb149af2b2431e
Lafllamme <teke900@gmail.com>
2025-05-17 19:15:11 +0200

    feat: Add InteractiveButton component with customizable props
    This commit introduces the InteractiveButton component in the UI library. The button supports customizable text and class properties and includes hover and focus animations for a dynamic user experience. Future enhancements, such as fixing the TODO syntax issue, can be implemented to further improve functionality.

---
+++ components/ui/Buttons/InteractiveButton/InteractiveButton.vue
@@ -0,0 +1,65 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+interface Props {
+  text?: string
+  class?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  text: 'Button',
+})
+
+const { class: classNames } = toRefs(props)
+
+const buttonRef = ref<HTMLButtonElement>()
+</script>
+
+<template>
+  <button
+    ref="buttonRef"
+    :class="
+      useClsx(
+        'bg-pureWhite dark:bg-pureBlack',
+        'rounded-full border border-gray-6 p-2 px-6 text-center font-semibold',
+        'group relative w-auto cursor-pointer overflow-hidden',
+        classNames,
+      )
+    "
+  >
+    <!-- TODO: Correct syntax of div here   -->
+    <div class="flex items-center gap-2">
+      <!-- 1st Content -->
+      <div
+        :class="useClsx(
+          'group-hover:scale-[100.8] group-focus:scale-[100.8] ',
+          'bg-pureBlack dark:bg-pureWhite transition-all duration-300',
+          'size-2 scale-100 rounded-lg',
+        )"
+      />
+      <span
+        :class="useClsx(
+          'inline-block whitespace-nowrap text-pureBlack dark:text-pureWhite ',
+          'transition-all duration-300 group-hover:translate-x-12',
+          'group-hover:opacity-0 group-focus:opacity-0',
+        )"
+      >
+        {{ text }}
+      </span>
+    </div>
+
+    <!-- 2nd Content -->
+    <div
+      :class="useClsx(
+        'text-primary-foreground opacity-0 transition-all duration-300',
+        'flex translate-x-12 items-center justify-center gap-2',
+        'group-hover:opacity-100 group-hover:-translate-x-5',
+        'group-focus:opacity-100 group-focus:-translate-x-5',
+        'absolute top-0 z-10 size-full',
+      )"
+    >
+      <span class="whitespace-nowrap">{{ text }}</span>
+      <Icon class="size-6" name="lucide:arrow-right" />
+    </div>
+  </button>
+</template>
