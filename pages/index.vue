19355d73dfcb1bf56a188b4c8e23783254f2834c
Lafllamme <teke900@gmail.com>
2025-05-17 17:28:31 +0200

    feat: Improve layout with responsive design adjustments
    Refactored layout to support better responsiveness, particularly for medium screens. Incorporated flexbox alignment, added width adjustments, and updated typography for improved readability and visual hierarchy. Animations were also refined for smoother transitions.

--- pages/index.vue
+++ pages/index.vue
@@ -33,16 +33,16 @@ watch(sceneLoaded, (v) => {
 </script>
 
 <template>
-  <!-- 1) push everything down by header’s height -->
+  <!-- 1) Main Container -->
   <div
     class="bg-pureWhite h-svh dark:bg-pureBlack"
   >
     <!-- 2) center & gutter vertical breathing room -->
     <div
-      class="flex flex-col items-center justify-center px-4 pt-20"
+      class="flex flex-col items-center justify-center px-4 pt-20 md:flex-row"
     >
-      <div class="mx-auto max-w-[600px] w-full">
-        <!-- optional max-w -->
+      <!-- Circle Wrapper -->
+      <div class="mx-auto max-w-[600px] w-full md:order-last md:max-w-[60rem] md:w-1/2 md:px-12">
         <div
           :class="useClsx(renderBackground ? 'bg-mint-8' : 'bg-mint-1')"
           class="relative aspect-square w-full overflow-hidden rounded-full transition-colors duration-[2000ms] ease-in-out"
@@ -55,17 +55,18 @@ watch(sceneLoaded, (v) => {
         </div>
       </div>
 
-      <div class="pt-6 text-center space-y-2">
+      <!-- Text Wrapper -->
+      <div class="w-full pt-6 text-center md:w-1/2 space-y-2 md:px-6 md:text-left">
         <h1
           :class="useClsx(
-            'geist-regular text-[8vw]',
+            'geist-regular text-[8vw] animate-clip-circle',
             'dark:text-pureWhite !font-bold',
             'text-pureBlack leading-tight tracking-tighter antialiased',
           )"
         >
           The Future of Tech is Here
         </h1>
-        <p class="text-gray-10 font-300">
+        <p class="text-md text-gray-10 font-300 md:text-justify lg:text-3xl md:text-2xl sm:text-lg">
           <TextGenerate
             :delay="0.8"
             :duration="1.1"
@@ -78,6 +79,19 @@ watch(sceneLoaded, (v) => {
 </template>
 
 <style scoped>
+@keyframes clipPathCircle {
+  0% {
+    clip-path: circle(0% at 50% 50%);
+  }
+  100% {
+    clip-path: circle(75% at 50% 50%);
+  }
+}
+
+.animate-clip-circle {
+  animation: clipPathCircle 3.2s cubic-bezier(0.77, 0, 0.175, 1) forwards;
+}
+
 @keyframes slide-in-bck-center {
   0% {
     transform: translateZ(600px);
