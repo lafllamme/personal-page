98c62e3f88ff26ee14b051c3cd4aa7053017ae4d
Lafllamme <teke900@gmail.com>
2025-05-18 13:14:06 +0200

    feat: Enhance RippleButton hover effects with smoother transitions
    Updated the RippleButton component to include a `transition-all` class, ensuring smoother hover transitions. This enhances the user experience by providing a more polished and visually appealing interaction.

--- components/ui/Buttons/RippleButton/RippleButton.vue
+++ components/ui/Buttons/RippleButton/RippleButton.vue
@@ -58,10 +58,11 @@ watchEffect(() => {
     ref="rippleButtonRef"
     :class="
       useClsx(
+        'hover:bg-gray-5A hover:shadow-[0_0_20px_rgba(255,255,255,0.5)] hover:border-pureWhite',
         'bg-pureWhite dark:bg-pureBlack p-2 px-6 text-center text-primary',
         'relative flex cursor-pointer items-center justify-center',
         'rounded-full border border-gray-6',
-        'hover:bg-gray-5A hover:shadow-[0_0_20px_rgba(255,255,255,0.5)] hover:border-pureWhite',
+        'transition-all duration-300',
         'font-semibold overflow-hidden',
         classNames,
       )
