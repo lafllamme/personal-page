7b2191e7052941ade1f1a2f5c0590b3ef4e9634e
Lafllamme <teke900@gmail.com>
2025-05-18 13:12:29 +0200

    feat: Enhance RippleButton hover styles for better UI feedback
    Added hover effects including background color change, shadow, and border color adjustment to improve the button's interactive feedback and visual appeal. This enhances user experience by providing clearer interaction cues.

--- components/ui/Buttons/RippleButton/RippleButton.vue
+++ components/ui/Buttons/RippleButton/RippleButton.vue
@@ -61,6 +61,7 @@ watchEffect(() => {
         'bg-pureWhite dark:bg-pureBlack p-2 px-6 text-center text-primary',
         'relative flex cursor-pointer items-center justify-center',
         'rounded-full border border-gray-6',
+        'hover:bg-gray-5A hover:shadow-[0_0_20px_rgba(255,255,255,0.5)] hover:border-pureWhite',
         'font-semibold overflow-hidden',
         classNames,
       )
