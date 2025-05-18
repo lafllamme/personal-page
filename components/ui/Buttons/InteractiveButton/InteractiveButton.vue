8256c2b05c50b1799323e4b0a2ab8aa7a1a4b218
Lafllamme <teke900@gmail.com>
2025-05-18 17:02:21 +0200

    feat: Add touch-manipulation class to InteractiveButton style
    This change improves touch responsiveness by adding the `touch-manipulation` class to the button styles. It ensures better handling of touch interactions, enhancing the overall user experience on touch devices.

--- components/ui/Buttons/InteractiveButton/InteractiveButton.vue
+++ components/ui/Buttons/InteractiveButton/InteractiveButton.vue
@@ -25,7 +25,7 @@ const buttonRef = ref<HTMLButtonElement>()
         'group relative w-auto cursor-pointer overflow-hidden',
         'rounded-full border border-gray-6 p-2 px-6',
         'bg-pureWhite dark:bg-pureBlack',
-        'text-center font-semibold',
+        'text-center font-semibold touch-manipulation',
         classNames,
       )
     "
