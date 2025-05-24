7bb20a5b8447311f8d47b209057fc411f37af757
Lafllamme <teke900@gmail.com>
2025-05-25 01:01:42 +0200

    feat: Refine Menu styles and enhance accessibility
    Updated Menu component styles for consistency, including rounded elements and improved transitions. Enhanced keyboard navigation, focus states, and hover effects to improve usability and accessibility. Adjusted fonts, icons, and spacing to align with design standards.

--- components/ui/Menu/Menu.vue
+++ components/ui/Menu/Menu.vue
@@ -174,7 +174,9 @@ useEventListener(window, 'keydown', handleEsc)
       <div class="relative z-10 h-full flex flex-col p-6 color-pureBlack h-svh dark:color-pureWhite">
         <!-- Search -->
         <div class="mb-10 mt-10">
-          <div class="relative flex items-center border border-solid px-4 py-2 color-pureBlack dark:color-pureWhite">
+          <div
+            class="relative flex items-center border rounded-3xl border-solid px-4 py-2 color-pureBlack dark:color-pureWhite"
+          >
             <input
               id="search"
               v-model="searchQuery"
@@ -190,6 +192,7 @@ useEventListener(window, 'keydown', handleEsc)
               @focus="handleInputFocus(true)"
             >
             <label
+              v-show="!searchQuery"
               :class="useClsx(
                 hasInputFocus && 'slide-out-blurred-top',
                 'color-gray-10 peer-focus:color-mint-8 transition-all duration-300 ease-out',
@@ -301,28 +304,37 @@ useEventListener(window, 'keydown', handleEsc)
             name="list"
             tag="div"
           >
-            <div v-for="(item, idx) in items" :key="item.id" class="border-b border-gray-7/30 last:border-b-0">
+            <div v-for="(item, idx) in items" :key="item.id">
               <button
                 :class="useClsx(
+                  idx !== 0 && 'border-t border-solid border-gray-5 dark:border-gray-2',
                   'group relative w-full flex items-center justify-between',
                   'color-pureBlack dark:color-pureWhite',
-                  'py-4 px-4 text-left text-3xl tracking-normal uppercase',
-                  'hover:text-white hover:bg-gradient-to-r hover:from-gray-8/20 hover:to-transparent',
-                  'transition-all duration-300 focus:outline-none',
+                  'py-4 px-4 text-left text-4xl tracking-normal uppercase hover:color-jade-11',
+                  'transition-colors duration-300 focus:outline-none focus:color-jade-11 focus-visible:bg-gray-5A rounded-full',
                 )"
                 @click="item.children && toggleItem(item.id)"
                 @keydown.enter.prevent="item.children && toggleItem(item.id)"
               >
+                <span
+                  class="group absolute left-0 top-1/2 h-8 w-1 scale-y-50 rounded-full bg-jade-11 opacity-0 transition-all duration-150 will-change-opacity will-change-transform -translate-y-1/2 group-focus:scale-y-100 group-hover:scale-y-100 group-focus:opacity-100 group-hover:opacity-100 group-focus-visible:!scale-y-0 group-focus-visible:!opacity-0"
+                />
+
                 <div class="flex items-center space-x-3">
-                  <span class="font-semibold tracking-wide">{{ item.title }}</span>
+                  <span
+                    class="font-semibold tracking-wide transition-all duration-300 group-focus:translate-x-3 group-hover:translate-x-3"
+                  >
+                    {{ item.title }}
+                  </span>
                 </div>
                 <Icon
                   v-if="item.children"
                   :class="useClsx(
-                    'transition-transform duration-300',
+                    'group-hover:color-jade-11A',
+                    'transition-all duration-300',
                     openItems.includes(item.id) ? 'rotate-45' : '',
                   )"
-                  class="h-6 w-6"
+                  class="h-6 w-6 md:h-8 md:w-8"
                   name="ri:add-large-fill"
                 />
               </button>
@@ -347,16 +359,16 @@ useEventListener(window, 'keydown', handleEsc)
                       :aria-hidden="openItems.includes(item.id) ? 'false' : 'true'"
                       :class="useClsx(
                         'flex items-center space-x-3 group',
-                        'w-full text-left px-4 py-2.5 text-sm',
-                        'text-gray-10 hover:text-gray-2',
-                        'hover:bg-gray-8/40 transition-all duration-200 rounded-lg',
+                        'w-full text-left px-4 py-2.5 text-md',
+                        'text-gray-10 hover:text-gray-12 focus-visible:text-gray-12',
+                        'transition-all duration-200 rounded-full',
                       )"
                       :tabindex="openItems.includes(item.id) ? 0 : -1"
                       :title="child.title"
                       :to="child.to || '/demo'"
                     >
                       <Icon
-                        class="h-3 w-3 text-gray-6 transition-colors duration-200 group-hover:text-mint-9"
+                        class="h-4 w-4 text-gray-6 transition-colors duration-200 group-focus-visible:color-mint-11 group-hover:color-mint-11"
                         name="ri:arrow-right-line"
                       />
                       <span>{{ child.title }}</span>
