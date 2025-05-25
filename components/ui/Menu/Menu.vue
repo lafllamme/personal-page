d919dd864c69c5e61b5d21b2cb52c868de958be9
Lafllamme <teke900@gmail.com>
2025-05-25 11:14:40 +0200

    feat: Refactor Menu footer into a reusable component
    Moved the footer implementation from `Menu.vue` to a new `Footer.vue` component for better modularity and code reusability. This change simplifies the Menu structure and promotes easier maintenance and scalability.

--- components/ui/Menu/Menu.vue
+++ components/ui/Menu/Menu.vue
@@ -2,6 +2,7 @@
 import type { MenuProps } from './Menu.model'
 import Link from '@/components/ui/Link/Link.vue'
 import MenuButton from '@/components/ui/Menu/Button/MenuButton.vue'
+import Footer from '@/components/ui/Menu/Footer/Footer.vue'
 import MenuSearch from '@/components/ui/Menu/Search/MenuSearch.vue'
 import { useMenu } from '@/stores/menu'
 import { useFocusTrap } from '@vueuse/integrations/useFocusTrap'
@@ -286,38 +287,7 @@ useEventListener(window, 'keydown', handleEsc)
           </TransitionGroup>
         </div>
         <!-- Footer -->
-        <div class="geist-regular mt-auto pt-6 text-xs md:text-sm">
-          <div
-            :class="useClsx(
-              'flex items-center justify-between border-t',
-              'border-gray-7A border-solid pt-6',
-            )"
-          >
-            <button
-              :class="useClsx(
-                'group focus:outline-none focus:ring focus:ring-mint-8 focus:ring-offset-2',
-                'transform transition-transform duration-300 ease-out hover:scale-105',
-                'transition-colors duration-300 ease-out hover:text-mint-11',
-                'ring-offset-pureWhite dark:ring-offset-pureBlack',
-              )"
-            >
-              <span>© 2025 TecNews</span>
-            </button>
-            <button
-              :class="useClsx(
-                'focus:ring-mint-8 dark:ring-offset-pureBlack',
-                'group flex items-center gap-1 tracking-wider uppercase',
-                'ring-offset-pureWhite focus:outline-none focus:ring focus:ring-offset-2',
-              )"
-            >
-              Contact
-              <Icon
-                class="h-4 w-4 transition-all duration-300 ease-out group-hover:translate-x-1.5 group-hover:color-mint-11"
-                name="ri:arrow-right-line"
-              />
-            </button>
-          </div>
-        </div>
+        <Footer />
       </div>
     </div>
   </div>
