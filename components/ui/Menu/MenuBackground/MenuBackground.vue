2f47fa4e17c528665f4191097fce62e754e43e1b
Lafllamme <teke900@gmail.com>
2025-05-25 13:26:41 +0200

    feat: Add MenuBackground component with animation handling
    Introduces the MenuBackground.vue component to manage menu background animations. Includes reactive animation handling triggered by menu state changes and visual enhancements using dynamic class bindings.

---
+++ components/ui/Menu/MenuBackground/MenuBackground.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import { useMenu } from '@/stores/menu'
+
+const menuStore = useMenu()
+const { isOpen } = storeToRefs(menuStore)
+
+const isAnimating = ref(false)
+
+function handleAnimation() {
+  isAnimating.value = true
+  useWait().then(() => {
+    isAnimating.value = false
+  })
+}
+
+watch(isOpen, (open) => {
+  if (open) {
+    handleAnimation()
+  }
+})
+</script>
+
+<template>
+  <div
+    :class="useClsx(
+      isAnimating && 'animated-fade-in-right',
+      'animate-delay-[50ms] animated  animated-duration-900 animated-repeat-1',
+    )"
+    class="pointer-events-none absolute inset-0"
+  >
+    <div
+      :class="useClsx(
+        'text-[18vh] color-gray-12A font-thin leading-none tracking-wider',
+        'absolute right-8 md:right-15 top-12 origin-right transform animate-glow',
+        'uppercase opacity-10 -translate-y-1/2 -rotate-90 font-electric',
+      )"
+    >
+      TecNews
+    </div>
+  </div>
+</template>
