9c6edac238b4610981c10d5fb32ef35f6517783a
Lafllamme <teke900@gmail.com>
2025-05-25 16:38:55 +0200

    feat: Add and manage menu items and last opened state
    Introduced a predefined set of menu items and functionality to handle a "last opened" state with setters and watchers. Enhanced `resetMenu` and `resetMenuStates` methods to optionally clear menu states. Added reactive watchers to monitor updates to menu items and open items for better debugging.

--- stores/menu/menu.ts
+++ stores/menu/menu.ts
@@ -10,10 +10,59 @@ export const useMenu = defineStore('menu', () => {
     consola.debug(`${infoPrint} ${message}`)
   }
 
+  // TODO: Move the menu items to a separate file or API call
+  const menuItems = [
+    {
+      id: 1,
+      title: 'Technology',
+      children: [
+        { id: 11, title: 'Gadgets', to: '/demo' },
+        { id: 12, title: 'Software', to: '/test' },
+        { id: 13, title: 'AI & Machine Learning', to: '/old' },
+      ],
+    },
+    {
+      id: 2,
+      title: 'Business',
+      children: [
+        { id: 21, title: 'Startups' },
+        { id: 22, title: 'Venture Capital' },
+        { id: 23, title: 'Market Trends' },
+      ],
+    },
+    {
+      id: 3,
+      title: 'Science',
+      children: [
+        { id: 31, title: 'Space' },
+        { id: 32, title: 'Environment' },
+        { id: 33, title: 'Health' },
+      ],
+    },
+    {
+      id: 4,
+      title: 'Reviews',
+      children: [
+        { id: 41, title: 'Smartphones' },
+        { id: 42, title: 'Laptops' },
+        { id: 43, title: 'Wearables' },
+      ],
+    },
+    {
+      id: 5,
+      title: 'About',
+    },
+    {
+      id: 6,
+      title: 'Contact',
+    },
+  ]
+  const items = ref<any[]>(menuItems)
+
   // Menu items and search functionality
-  const items = ref<any[]>([])
   const openItems = ref<any[]>([])
   const searchQuery = ref('')
+  const lastOpened = ref<object | null>(null)
 
   // Setters for items and search query
   function setItems(newItems: any[]) {
@@ -26,6 +75,25 @@ export const useMenu = defineStore('menu', () => {
 
   function toggleOpenItem(id: number) {
     openItems.value = openItems.value[0] === id ? [] : [id]
+  }
+
+  function setLastOpened(item: object) {
+    lastOpened.value = item
+  }
+
+  function clearLastOpened() {
+    lastOpened.value = null
+  }
+
+  function handleLastOpened(state: 'set' | 'clear', openItem: object) {
+    if (state === 'set' && typeof openItem !== 'undefined' && openItem) {
+      setLastOpened(openItem)
+      log(`Last opened item set to ${lastOpened.value}`)
+    }
+    else if (state === 'clear') {
+      clearLastOpened()
+      log('Last opened item cleared')
+    }
   }
 
   function setSearchQuery(query: string) {
@@ -41,15 +109,17 @@ export const useMenu = defineStore('menu', () => {
     isOpen.value = true
   }
 
-  function resetMenu() {
-    setOpenItems([])
+  function resetMenu(clearMenu: boolean) {
+    if (clearMenu) {
+      setOpenItems([])
+    }
     searchQuery.value = ''
     log('Menu closed, search query cleared')
   }
 
-  function resetMenuStates(timeout?: number) {
+  function resetMenuStates(timeout?: number, clearMenu = true) {
     const wait = new Promise(resolve => setTimeout(resolve, timeout || 300))
-    wait.then(() => resetMenu())
+    wait.then(() => resetMenu(clearMenu))
   }
 
   function toggleMenu(state: 'open' | 'close' | 'toggle') {
@@ -110,6 +180,14 @@ export const useMenu = defineStore('menu', () => {
     }
   })
 
+  watch(items, (v) => {
+    consola.debug('[Menu Store] Items updated:', v)
+  }, { immediate: true })
+
+  watch(openItems, (v) => {
+    consola.debug('[Menu Store] Open items updated:', v)
+  })
+
   return {
     // Menu state
     isOpen,
@@ -122,6 +200,8 @@ export const useMenu = defineStore('menu', () => {
     setItems,
     openItems,
     setOpenItems,
+    handleLastOpened,
+    lastOpened,
     toggleOpenItem,
     showAccordion,
 
