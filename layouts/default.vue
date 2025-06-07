49e200452e607b92c2439f1db6f4562ee58ff33f
Lafllamme <teke900@gmail.com>
2025-06-07 23:59:31 +0200

    refactor: Add top margin to main content in DefaultLayout for consistent spacing across breakpoints

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,7 @@ import Header from '@/components/ui/Header/Header.vue'
       class="relative z-10 overflow-y-auto bg-pureWhite transition-colors duration-600 ease-[cubic-bezier(0.33,1,0.68,1)] dark:bg-pureBlack"
     >
       <Header />
-      <main>
+      <main class="mt-[calc(3rem+1px)] md:mt-[calc(3.5rem+1px)]">
         <slot name="default" />
       </main>
     </div>
