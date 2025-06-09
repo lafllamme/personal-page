01ba14bd232a129febee5d8de8509298e0c4ec84
Lafllamme <teke900@gmail.com>
2025-06-09 18:19:51 +0200

    refactor: Update `color-mode` storage to use cookies instead of localStorage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,7 +59,7 @@ export default defineNuxtConfig({
     componentName: 'ColorScheme',
     classPrefix: '',
     classSuffix: '',
-    storage: 'localStorage', // or 'sessionStorage' or 'cookie'
+    storage: 'cookie', // or 'sessionStorage' or 'cookie'
     storageKey: 'color-mode',
   },
   css: [
