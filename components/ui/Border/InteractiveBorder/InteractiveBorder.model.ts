564f26eb82d279676d9c6efd82daabf79917adb8
Lafllamme <teke900@gmail.com>
2025-06-09 23:59:21 +0200

    refactor: Add `InteractiveBorder` component with props model and default styling
    - Introduce `InteractiveBorder.vue` for managing complex border animations.
    - Define `InteractiveBorderProps` model with default props for customization.
    - Implement dynamic styles, mouse movement handling, and proximity-based interactions.
    - Ensure reusability and maintainability through extracted logic and default values.

---
+++ components/ui/Border/InteractiveBorder/InteractiveBorder.model.ts
@@ -0,0 +1,26 @@
+import type { HTMLAttributes } from 'vue'
+
+export interface InteractiveBorderProps {
+  blur?: number
+  inactiveZone?: number
+  proximity?: number
+  spread?: number
+  variant?: 'default' | 'white'
+  glow?: boolean
+  class?: HTMLAttributes['class']
+  disabled?: boolean
+  movementDuration?: number
+  borderWidth?: number
+}
+
+export const InteractiveBorderDefaultProps: InteractiveBorderProps = {
+  blur: 0,
+  inactiveZone: 0.7,
+  proximity: 0,
+  spread: 20,
+  variant: 'default',
+  glow: false,
+  movementDuration: 2,
+  borderWidth: 1,
+  disabled: true,
+}
