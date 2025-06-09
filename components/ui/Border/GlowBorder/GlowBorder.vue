25a763c222e1acc7944c1148d83cc8dcd732abf4
Lafllamme <teke900@gmail.com>
2025-06-09 17:43:27 +0200

    refactor: Enhance `GlowBorder` visibility handling with dynamic refs and conditional styles
    - Integrate `useVisibilityObserver` with dynamic refs for visibility tracking.
    - Add conditional styles and animations based on visibility.
    - Remove redundant static class references and simplify computed properties.

--- components/ui/Border/GlowBorder/GlowBorder.vue
+++ components/ui/Border/GlowBorder/GlowBorder.vue
@@ -8,28 +8,40 @@ const props = withDefaults(
 )
 
 const { borderRadius, borderWidth, duration, color, class: classNames } = toRefs(props)
+const isVisible = ref(false)
+const glowBorderRef = useTemplateRef('glowBorderRef')
+const isCurrentlyVisible = useVisibilityObserver(glowBorderRef, isVisible, 30, true)
 
-const parentStyles = computed(() => {
-  return { '--border-radius': `${borderRadius.value}px` }
-})
+const parentStyles = computed(() => (
+  isCurrentlyVisible?.value ? { '--border-width': `${borderWidth.value}px` } : {}
+))
 
-const childStyles = computed(() => ({
-  '--border-width': `${borderWidth.value}px`,
-  '--border-radius': `${borderRadius.value}px`,
-  '--glow-pulse-duration': `${duration.value}s`,
-  '--mask-linear-gradient': `linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)`,
-  '--background-radial-gradient': `radial-gradient(circle, transparent, ${
-    Array.isArray(color.value) ? color.value.join(',') : color.value
-  }, transparent)`,
-}))
+const childStyles = computed(() => (
+  isCurrentlyVisible?.value
+    ? {
+        '--border-width': `${borderWidth.value}px`,
+        '--border-radius': `${borderRadius.value}px`,
+        '--glow-pulse-duration': `${duration.value}s`,
+        '--mask-linear-gradient': `linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)`,
+        '--background-radial-gradient': `radial-gradient(circle, transparent, ${
+          Array.isArray(color.value) ? color.value.join(',') : color.value
+        }, transparent)`,
+      }
+    : {}
+))
+
+const borderAnimation = computed(() => isCurrentlyVisible?.value ? 'glow-border' : '')
 </script>
 
 <template>
   <div
+    ref="glowBorderRef"
     :class="
       useClsx(
-        'relative grid min-h-[60px] w-fit min-w-[300px] place-items-center  rounded-bl-[42px] rounded-br-[38px] rounded-tl-[36px] rounded-tr-[40px] glow-border',
+        'relative grid min-h-[60px] w-fit min-w-[300px] place-items-center',
+        'rounded-bl-[42px] rounded-br-[38px] rounded-tl-[36px] rounded-tr-[40px]',
         classNames,
+        borderAnimation,
       )
     "
     :style="parentStyles"
@@ -37,8 +49,9 @@ const childStyles = computed(() => ({
     <div
       :class="
         useClsx(
+          borderAnimation,
           'before:rounded-bl-[42px] before:rounded-br-[38px] before:rounded-tl-[36px] before:rounded-tr-[40px]',
-          'glow-border before:absolute before:inset-0 before:aspect-square before:size-full before:bg-[length:300%_300%]',
+          'before:absolute before:inset-0 before:aspect-square before:size-full before:bg-[length:300%_300%]',
           `before:p-[--border-width] before:opacity-50 before:will-change-[background-position] before:content-['']`,
           'before:![-webkit-mask-composite:xor] before:![mask-composite:exclude] before:[mask:--mask-linear-gradient] pointer-events-none',
         )
@@ -57,13 +70,13 @@ const childStyles = computed(() => ({
 
 @keyframes glow-pulse {
   0% {
-    background-position: 0% 0%;
+    background-position: 0 0;
   }
   50% {
     background-position: 100% 100%;
   }
   100% {
-    background-position: 0% 0%;
+    background-position: 0 0;
   }
 }
 </style>
