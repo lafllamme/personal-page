510dfe258b0e42cfda05129a88682d442d0e2227
Lafllamme <teke900@gmail.com>
2025-06-09 23:59:29 +0200

    feat: Add `InteractiveCard` component with default props and styling
    - Introduce `InteractiveCard.vue` for creating reusable interactive card designs.
    - Incorporate `InteractiveBorder` for enhanced border animations.
    - Define default props and class management via `InteractiveCard.model.ts`.
    - Implement slots for flexible content rendering.

---
+++ components/ui/Border/InteractiveCard/InteractiveCard.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import type { InteractiveCardProps } from './InteractiveCard.model'
+import InteractiveBorder from '@/components/ui/Border/InteractiveBorder/InteractiveBorder.vue'
+import { InteractiveCardDefaultProps } from './InteractiveCard.model'
+
+const props = withDefaults(defineProps<InteractiveCardProps>(), InteractiveCardDefaultProps)
+
+const { containerClass, contentClass } = toRefs(props)
+
+consola.debug(containerClass, contentClass)
+</script>
+
+<template>
+  <div
+    :class="useClsx(
+      'border border-gray-6 border-solid',
+      'rounded-2.5xl md:rounded-3xl',
+      'relative h-full',
+      'md:p-3 p-2',
+    )"
+  >
+    <InteractiveBorder
+      :disabled="false"
+      :glow="true"
+      :inactive-zone="0.01"
+      :proximity="64"
+      :spread="40"
+    />
+    <div
+      :class="useClsx(
+        'dark:shadow-[0px_0px_27px_0px_#2D2D2D]',
+        'bg-pureWhite dark:bg-pureBlack',
+        'overflow-hidden border-0.75',
+        'rounded-xl p-6 md:p-6',
+      )"
+    >
+      <slot name="default" />
+    </div>
+  </div>
+</template>
