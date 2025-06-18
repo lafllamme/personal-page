d180e012eaba1fb15e027f1e180341a478d6710f
Lafllamme <teke900@gmail.com>
2025-06-18 12:58:04 +0200

    feat: Revamp `TextScrollReveal` styles and add neon glow animation
    - Adjust font styles for improved readability (`bruno-ace-sc-regular`) and consistent tracking.
    - Modify text size scaling within clamp for better responsiveness.
    - Introduce neon glow effect with animations for enhanced visual appeal on specific words.
    - Refine layout with `max-w-8xl` for better content alignment.

--- components/ui/Scroll/TextScrollReveal/TextScrollReveal.vue
+++ components/ui/Scroll/TextScrollReveal/TextScrollReveal.vue
@@ -39,17 +39,18 @@ useEventListener('resize', updateScrollYProgress)
       classNames,
     )"
   >
-    <div class="sticky top-0 flex items-center bg-transparent py-12 h-svh md:py-20">
+    <div class="max-w-8xl sticky top-0 flex items-center bg-transparent py-12 h-svh md:py-20">
       <p
         :class="useClsx(
           'color-mint-12A',
           'flex flex-wrap antialiased',
-          'font-semibold leading-tight tracking-tight font-recoleta text-pretty text-[clamp(2.2rem,9vw,6rem)]',
+          'font-semibold leading-tight tracking-normal bruno-ace-sc-regular text-pretty text-[clamp(3rem,10vw,7rem)]',
         )"
       >
         <ScrollWord
           v-for="(word, i) in words"
           :key="i"
+          :class="(i === 4 && scrollYProgress > 0.5) && 'neon-glow color-purple-10'"
           :progress="scrollYProgress"
           :range="[i / words.length, (i + 1) / words.length]"
           :word="word"
@@ -58,3 +59,28 @@ useEventListener('resize', updateScrollYProgress)
     </div>
   </div>
 </template>
+
+<style>
+@keyframes glow-pulse-purple {
+  from {
+    text-shadow:
+      0 0 16px rgba(167, 139, 250, 0.64),
+      /* slightly more blur, more alpha */ 0 0 32px rgba(139, 92, 246, 0.42),
+      0 0 40px rgba(232, 121, 249, 0.18);
+  }
+  to {
+    text-shadow:
+      0 0 22px rgba(167, 139, 250, 0.82),
+      0 0 40px rgba(139, 92, 246, 0.56),
+      0 0 52px rgba(232, 121, 249, 0.32);
+  }
+}
+
+.neon-glow {
+  animation: glow-pulse-purple 2s ease-in-out infinite alternate;
+  text-shadow:
+    0 0 16px rgba(167, 139, 250, 0.64),
+    0 0 32px rgba(139, 92, 246, 0.42),
+    0 0 40px rgba(232, 121, 249, 0.18);
+}
+</style>
