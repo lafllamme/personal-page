9bd483dcf648050f585e76f695b636bd14f04ca2
Lafllamme <teke900@gmail.com>
2025-06-18 02:25:57 +0200

    feat: Replace `TextScrollReveal` with `ReviewMarquee` in `demo.vue`
    - Swap out `TextScrollReveal` for `ReviewMarquee` to update the component display.
    - Simplify container styling for cleaner layout.

--- pages/demo.vue
+++ pages/demo.vue
@@ -1,11 +1,9 @@
 <script lang="ts" setup>
-import TextScrollReveal from '@/components/ui/Scroll/TextScrollReveal/TextScrollReveal.vue'
+import ReviewMarquee from '@/components/ui/ReviewMarquee/ReviewMarquee.vue'
 </script>
 
 <template>
-  <div
-    class="z-10 min-h-64 flex items-center justify-center border rounded-lg bg-pureWhite dark:bg-pureBlack"
-  >
-    <TextScrollReveal text="Mein Boo ist die ALLER SCHÖNSTE BOO DER WELT." />
+  <div class="bg-puraeWhite dark:bg-pureBlack">
+    <ReviewMarquee />
   </div>
 </template>
