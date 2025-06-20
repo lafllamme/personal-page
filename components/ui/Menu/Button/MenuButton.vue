b0eb41eef4940ae24e36013a13fc07a716d70c7e
Lafllamme <teke900@gmail.com>
2025-06-20 20:16:14 +0200

    feat: Update styles in `MenuButton.vue` for improved responsiveness and visual consistency

--- components/ui/Menu/Button/MenuButton.vue
+++ components/ui/Menu/Button/MenuButton.vue
@@ -18,7 +18,7 @@ const { toggleMenu } = menuStore
     :class="useClsx(
       'group focus-visible:ring-pureBlack dark:focus-visible:ring-pureWhite focus-visible:ring-inset focus-visible:ring',
       'transition-transform focus:outline-none ease-in-out duration-300',
-      'absolute z-100 menu absolute right-4 top-1 z-100 md:top-2',
+      'absolute z-100 top-1.5 md:top-1.75 -translate-x-full absolute z-100',
       isOpen && 'opened',
     )"
     aria-controls="mobile-menu"
@@ -27,19 +27,19 @@ const { toggleMenu } = menuStore
     @click="toggleMenu('toggle')"
   >
     <svg
-      class="h-10 w-10 transition-colors duration-600 ease-[cubic-bezier(0.33,1,0.68,1)]"
+      class="size-6 transition-colors duration-600 ease-[cubic-bezier(0.33,1,0.68,1)] md:size-8"
       viewBox="0 0 100 100"
     >
       <path
-        class="line line1 stroke-pureBlack dark:stroke-pureWhite"
+        class="line line1 transform-origin-center scale-135 stroke-pureBlack dark:stroke-pureWhite"
         d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
       />
       <path
-        class="line line2 stroke-pureBlack dark:stroke-pureWhite"
+        class="line line2 transform-origin-center scale-135 stroke-pureBlack dark:stroke-pureWhite"
         d="M 20,50 H 80"
       />
       <path
-        class="line line3 stroke-pureBlack dark:stroke-pureWhite"
+        class="line line3 transform-origin-center scale-135 stroke-pureBlack dark:stroke-pureWhite"
         d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
       />
     </svg>
