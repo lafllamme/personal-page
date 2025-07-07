2cb4cbfd7c396d093cac96214e803c2b7984d94d
Lafllamme <teke900@gmail.com>
2025-07-07 22:18:29 +0200

    feat: fetch articles from Strapi in demo.vue and display response

--- pages/demo.vue
+++ pages/demo.vue
@@ -1,8 +1,16 @@
 <script lang="ts" setup>
 import ReviewMarquee from '@/components/ui/ReviewMarquee/ReviewMarquee.vue'
+
+const { find } = useStrapi()
+const response = await find('articles')
+
+consola.debug('[Demo] Received response:', response.data)
 </script>
 
 <template>
+  <div class="color-red-12">
+    {{ response }}
+  </div>
   <div class="bg-pureWhite dark:bg-pureBlack">
     <ReviewMarquee />
   </div>
