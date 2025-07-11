670780712da9db3823e677a1ec380e965246a630
Lafllamme <teke900@gmail.com>
2025-07-11 11:58:13 +0200

    refactor: remove unused Strapi fetch logic from demo.vue

--- pages/demo.vue
+++ pages/demo.vue
@@ -1,16 +1,8 @@
 <script lang="ts" setup>
 import ReviewMarquee from '@/components/ui/ReviewMarquee/ReviewMarquee.vue'
-
-const { find } = useStrapi()
-const response = await find('articles')
-
-consola.debug('[Demo] Received response:', response.data)
 </script>
 
 <template>
-  <div class="color-red-12">
-    {{ response }}
-  </div>
   <div class="bg-pureWhite dark:bg-pureBlack">
     <ReviewMarquee />
   </div>
