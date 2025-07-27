3898d51465d35f479d0a89addb086026e1cdcc90
Lafllamme <teke900@gmail.com>
2025-07-27 15:24:38 +0200

    refactor: update image URLs in BlogLayout to use Imgur sources
    - Replace Unsplash image URLs with Imgur equivalents for better accessibility and consistency

--- frontend/components/ui/Blog/Layout/BlogLayout.vue
+++ frontend/components/ui/Blog/Layout/BlogLayout.vue
@@ -14,12 +14,12 @@ const data = [
   {
     category: 'Artificial Intelligence',
     title: 'You can do more with AI.',
-    src: 'https://images.unsplash.com/photo-1593508512255-86ab42a8e620?q=80&w=3556&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    src: 'https://i.imgur.com/9EOfmtX.jpeg',
   },
   {
     category: 'Productivity',
     title: 'Enhance your productivity.',
-    src: 'https://images.unsplash.com/photo-1531554694128-c4c6665f59c2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    src: 'https://i.imgur.com/OdC4wBG.jpeg',
     // content: <DummyContent />,
   },
   {
@@ -32,17 +32,17 @@ const data = [
   {
     category: 'Product',
     title: 'Maps for your iPhone 15 Pro Max.',
-    src: 'https://images.unsplash.com/photo-1599202860130-f600f4948364?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    src: 'https://i.imgur.com/B1BZH7P.jpeg',
   },
   {
     category: 'iOS',
     title: 'Photography just got better.',
-    src: 'https://images.unsplash.com/photo-1602081957921-9137a5d6eaee?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    src: 'https://i.imgur.com/OxgUcYO.jpeg',
   },
   {
     category: 'Hiring',
     title: 'Hiring for a Staff Software Engineer',
-    src: 'https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    src: 'https://i.imgur.com/UnI4RkM.jpeg',
   },
 ]
 const featuredArticle = reactive({
