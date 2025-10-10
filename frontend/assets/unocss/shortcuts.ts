51aeae8d344adea4a9f55171e08fb5f54e404368
Lafllamme <teke900@gmail.com>
2025-10-10 16:45:29 +0200

    fix: update text-shadow values for improved contrast in light and dark modes

--- frontend/assets/unocss/shortcuts.ts
+++ frontend/assets/unocss/shortcuts.ts
@@ -2,6 +2,6 @@ export const shortcuts = [
   // ['btn', 'px-4 py-2 rounded bg-primary text-white'],
   [
     'ts-contrast',
-    '[text-shadow:0_0_8px_rgba(0,0,0,0.4),_0_0_16px_rgba(0,0,0,0.2)] dark:[text-shadow:0_0_8px_rgba(255,255,255,0.4),_0_0_16px_rgba(255,255,255,0.2)]',
+    '[text-shadow:0_0_8px_rgba(0,0,0,0.23),_0_0_16px_rgba(0,0,0,0.12)] dark:[text-shadow:0_0_8px_rgba(255,255,255,0.23),_0_0_16px_rgba(255,255,255,0.12)]',
   ],
 ]
