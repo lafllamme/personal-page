b9edf85c7b987761a02e822b01742ae1d4967734
Lafllamme <teke900@gmail.com>
2025-11-03 21:00:03 +0100

    feat: adjust `SpiralControls` spacing and positioning for improved alignment and layout consistency

--- frontend/components/ui/Card/Spiral/SpiralControls.vue
+++ frontend/components/ui/Card/Spiral/SpiralControls.vue
@@ -24,7 +24,7 @@ function handleNext() {
 </script>
 
 <template>
-  <div class="jetbrains-mono-regular absolute right-8 top-20 z-40 flex flex-col items-end md:top-22 space-y-3 md:space-y-6">
+  <div class="jetbrains-mono-regular absolute right-4 top-5 z-40 flex flex-col items-end md:right-6 space-y-3 md:space-y-6">
     <!-- Floating controls for navigating the spiral -->
     <div
       :class="useClsx(
