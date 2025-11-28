ded2fd8bd65d0dc4a1ede88860296476a7faa5de
Lafllamme <teke900@gmail.com>
2025-11-28 23:45:51 +0100

    feat: add `PageContainer` Vue component with type definitions and default props configuration

---
+++ frontend/app/components/ui/Partials/PageContainer/PageContainer.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { PageContainerProps } from './PageContainer.model'
+import { toRefs } from 'vue'
+import {
+  PageContainerDefaults,
+  PageContainerGutterClasses,
+  PageContainerMaxClasses,
+} from './PageContainer.model'
+
+const props = withDefaults(defineProps<PageContainerProps>(), PageContainerDefaults)
+const { as: tag, max, gutter, bleed } = toRefs(props)
+</script>
+
+<template>
+  <component
+    :is="tag"
+    :class="useClsx(
+      'w-full relative',
+      !bleed && PageContainerMaxClasses[max],
+      !bleed && PageContainerGutterClasses[gutter],
+    )"
+  >
+    <slot name="default" />
+  </component>
+</template>
