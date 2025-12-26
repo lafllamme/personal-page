fe2c15d7412e1b37db53404235396ded11f0b759
Lafllamme <teke900@gmail.com>
2025-12-26 12:23:13 +0100

    fix: remove redundant ESLint disable comment in `InteractiveBorder` component

--- frontend/app/components/ui/Border/InteractiveBorder/InteractiveBorder.vue
+++ frontend/app/components/ui/Border/InteractiveBorder/InteractiveBorder.vue
@@ -83,7 +83,7 @@ useEventListener(document, 'pointermove', handlePointerMove, { passive: true })
 
 onMounted(() => {
   if (disabled.value)
-  // eslint-disable-next-line no-useless-return
+   
     return
 })
 
