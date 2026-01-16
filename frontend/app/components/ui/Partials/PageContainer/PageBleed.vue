362000200eba51baee422d30b21b1dfbdac389f6
Lafllamme <teke900@gmail.com>
2026-01-16 16:47:46 +0100

    feat: introduce `PageBleed` component and update Liquid page layout
    - Added `PageBleed` component to enhance layout flexibility with gutter options.
    - Refactored Liquid page to use `PageBleed` for improved alignment and cleaner code.
    - Updated `PageContainer` to support dynamic props for better reusability.

---
+++ frontend/app/components/ui/Partials/PageContainer/PageBleed.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { PageContainerGutter, PageContainerTag } from './PageContainer.model'
+import { toRefs } from 'vue'
+import { PageContainerBleedClasses, PageContainerDefaults } from './PageContainer.model'
+
+const props = withDefaults(defineProps<{
+  as?: PageContainerTag
+  gutter?: PageContainerGutter
+}>(), {
+  as: 'div',
+  gutter: PageContainerDefaults.gutter,
+})
+
+const { as: tag, gutter } = toRefs(props)
+</script>
+
+<template>
+  <component
+    :is="tag"
+    :class="useClsx(
+      'relative',
+      PageContainerBleedClasses[gutter],
+    )"
+  >
+    <slot name="default" />
+  </component>
+</template>
