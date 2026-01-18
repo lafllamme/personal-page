26a0fb444763b33bb57ea3b1743f52a00a2efb3e
Lafllamme <teke900@gmail.com>
2026-01-18 16:15:59 +0100

    feat: implement OsmoHeader component with dynamic menu and interactions
    - Added `OsmoHeader` component featuring dynamic menu animations and scroll-based interactions.
    - Integrated reusable subcomponents (`OsmoLogo`, `OsmoLogoMark`, `OsmoMenuIcon`, `OsmoScrambleTextButton`) for cleaner and modular design.
    - Introduced models (`OsmoHeader.model.ts`) for structured menu item, avatar, and social link data.
    - Incorporated visual enhancements using `motion-v` and responsive layout updates.

---
+++ frontend/app/components/ui/OsmoHeader/OsmoLogo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import logoOsmo from '@/assets/logos/logo-osmo.svg'
+</script>
+
+<template>
+  <div class="relative h-9 md:h-10 xl:h-12 w-auto aspect-video">
+    <NuxtImg
+      :src="logoOsmo"
+      alt="OSMO"
+      class="h-full w-full"
+      loading="eager"
+    />
+  </div>
+</template>
---
+++ frontend/app/components/ui/OsmoHeader/OsmoLogoMark.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import logoMark from '@/assets/logos/logo-mark.svg'
+</script>
+
+<template>
+  <div class="relative size-6 md:size-7">
+    <NuxtImg
+      :src="logoMark"
+      alt="OSMO"
+      class="h-full w-full"
+      loading="eager"
+    />
+  </div>
+</template>
